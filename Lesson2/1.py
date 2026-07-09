a3d36ea1b5b33b7b089a6f255b51cfcf622abf8e
shettyaarav23-dot <shetty.aarav23@gmail.com>
2026-07-09 12:22:58 +0400

    lesson2 commpleted

---
+++ Lesson2/1.py
@@ -0,0 +1,7 @@
+print("welcome to the world of python")
+x=10
+print("the value of x", x)
+print("the value of x is x")
+print("x")
+print(x)
+print(f"the value of x is {x}")
---
+++ Lesson2/2.py
@@ -0,0 +1,9 @@
+x=10
+y=7
+z="aarav"
+print("the value of x is",x)
+print(f"the value of x is {x}")
+print("x+y")
+print(x+y)
+print(f"the addition of x+y is {x+y}")
+print("the division of x/y is", x/y)

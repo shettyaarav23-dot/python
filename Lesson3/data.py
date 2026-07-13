5e8c04148761465b7a09092edddde611e3dbba09
shettyaarav23-dot <shetty.aarav23@gmail.com>
2026-07-13 14:35:49 +0400

    Lesson3 completed

---
+++ Lesson3/data.py
@@ -0,0 +1,12 @@
+a=15
+b=32.34
+c="aarav"
+d=True
+print("the value of a is", a)
+print("the value of b is", b)
+print(c)
+print("the answer is", d)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
---
+++ Lesson3/type.py
@@ -0,0 +1,17 @@
+name=input("what is your name")
+age=int(input("what is your age"))
+marks=float(input("what were your marks"))
+student=bool(input("are you a student"))
+print(name)
+print(age)
+print(marks)
+print(student)
+
+a=10.75
+b=int(a)
+print(a)
+print(b)
+n="2"
+c=int(n)
+print(c)
+print(n)

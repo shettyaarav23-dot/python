4cf5d241622f81949ac73bafe138824f526d39a3
shettyaarav23-dot <shetty.aarav23@gmail.com>
2026-07-13 15:15:19 +0400

    lesson4 completed

---
+++ Lesson4/harvest.py
@@ -0,0 +1,27 @@
+field1=50
+field2=30
+field3=24
+field4=78
+field5=92
+total=field1+field2+field3+field4+field5
+average=total/5
+print("total sum of all 5 observation" ,total)
+print("average sum is" ,average)
+price=100
+a=(price*total)
+print("the multiplication is", a)
+bag= total // 25
+leftover= total%25
+print("the floor division is" ,bag)
+print("the remendier is" ,leftover)
+b=34
+c=45
+print(b==c)
+print(b!=c)
+print(b>=c)
+print(b<=c)
+print(b>c)
+print(b<c)
+print(b+3)
+b+=3
+print(b)

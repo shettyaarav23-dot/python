698d1620f0c8133d00e5e48af7bd7a0ab2bb97e2
shettyaarav23-dot <shetty.aarav23@gmail.com>
2026-07-14 11:49:15 +0400

    birthday completed

---
+++ Lesson2/birthday.py
@@ -0,0 +1,10 @@
+birthday1= "12/01/2011"
+birthday2= "25/03/2011"
+birthday3= "18/07/2011"
+birthday4= "09/11/2011"
+birthday5= "30/12/2010"
+print(birthday1)
+print(birthday2)
+print(birthday3)
+print(birthday4)
+print(birthday5)

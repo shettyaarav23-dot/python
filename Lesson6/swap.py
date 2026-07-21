52d044bfa1678e7b8261595b45001fd8856c043a
shettyaarav23-dot <shetty.aarav23@gmail.com>
2026-07-21 12:14:16 +0400

    Lesson6 completed

---
+++ Lesson6/swap.py
@@ -0,0 +1,15 @@
+a = int(input("enter the first number"))
+b = int(input("enter the second number"))
+c = int(input("enter the third number"))
+
+print("before swapping")
+print("a =" ,a)
+print("b =" ,b)
+print("c =" ,c)
+
+a,b,c = b,c,a
+
+print("after swapping")
+print("a =" ,a)
+print("b =" ,b)
+print("c =" ,c)

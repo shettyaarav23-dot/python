6c5d523f46c1de3c86398f680cfcf91e2865154f
shettyaarav23-dot <shetty.aarav23@gmail.com>
2026-07-23 12:21:45 +0400

    Lesson8 completed

---
+++ Lesson8/reverse.py
@@ -0,0 +1,3 @@
+number=int(input("the the number"))
+for i in range(number,0,-1):
+    print(i)
---
+++ Lesson8/sum.py
@@ -0,0 +1,5 @@
+number=int(input("enter the number"))
+v=0
+for i in range(0,number+1):
+    v=v+i
+print(v)
